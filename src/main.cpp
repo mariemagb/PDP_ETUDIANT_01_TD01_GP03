7d2319a9a9cebab8098e98df1860012a44806023
MarieMag <mariemag.bonnet@gmail.com>
2024-01-22 10:09:02 +0100

    fix etape2

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,39 @@
 #include <Arduino.h>
+#include "DHT.h"
+
+#define SENSOR 33
+#define LED 26
+DHT dht(SENSOR, DHT11);
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  
+  dht.begin();
+
+  pinMode(LED, OUTPUT);
 }
 
 void loop() {
-  // put your code here to run repeatedly
+  delay(5000);
+
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+
+  if (isnan(humidity) || isnan(temperature)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  if (temperature > 25) {
+    digitalWrite(LED, HIGH);
+  } else {
+    digitalWrite(LED, LOW);
+  }
+
+  Serial.print("Humidity: ");
+  Serial.print(humidity);
+  Serial.println("%");
+  Serial.print("Temperature: ");
+  Serial.print(temperature);
+  Serial.println("°C");
 }
